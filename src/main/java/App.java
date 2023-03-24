a716c212a6795a64a0333d43c38658e1c4f0e7a6
Tipo-bit <gorvat2012@yandex.ru>
2023-03-24 19:26:37 +0300

    Hello

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,25 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        System.out.println("Ссылаются на один объект? " + (bean == bean1));
+
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getMessage());
+
+        System.out.println("Ссылаются на один объект? " + (cat == cat1));
     }
 }
